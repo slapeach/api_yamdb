147cc6950e4b69ea3991416f3bf6e92a96965785
Alena Zavgorodniaia <alzania@yandex.ru>
2022-04-11 21:10:22 +0300

    test #4

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-from django.db import models
-<<<<<<< HEAD
-
-
-print('И тебе привет!!!')
-=======
-Privet
-# Create your models here.
->>>>>>> e719c6f7bae0a1d7545c17b6832e8d351f85f9fe
-=======
-# New_string
->>>>>>> c84006114e415772fc7dd2ce141e10696cf7f623
+:D
