d8407aaad63c784160337e23e56e6fe7e166aa66
Третьяков Николай <nikontra@yandex.ru>
2022-04-11 23:17:53 +0300

    Edit

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-:D
-=======
-<<<<<<< HEAD
-from django.db import models
-<<<<<<< HEAD
-
+from django.db import 
 
 print('И тебе привет!!!')
-=======
 Privet
 # Create your models here.
->>>>>>> e719c6f7bae0a1d7545c17b6832e8d351f85f9fe
-=======
 # New_string
->>>>>>> c84006114e415772fc7dd2ce141e10696cf7f623
 print("Что-то")
->>>>>>> 3e2b96bd0729d6eb466254bc90f664b093fb41ba
