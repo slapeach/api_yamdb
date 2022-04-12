4f0016eb8266a70916bc7ce0c3c60a796fd2f55a
Алексей Жигалов <avzh79@gmail.com>
2022-04-12 19:49:09 +0300

    Permissions created

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,46 @@
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """Пермишен для доступа  к изменению контента
+       только модераторам или авторам"""
+
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
+
+
+class IsModeratorOrReadOnly(permissions.BasePermission):
+    """Пермишен для доступа  к изменению контента
+       только модераторам или авторам"""
+
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.role == 'moderator'
+        )
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """Пермишен для доступа  к изменению контента
+       только модераторам или авторам"""
+
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_staff
+            or request.user.is_superuser
+            or request.user.role == 'admin'
+        )
+
+
+
+
+
+
+
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import ReviewViewSet, CommentViewSet
+from .views import ReviewViewSet, CommentViewSet, UserViewSet
 
 app_name = 'api'
 
@@ -14,6 +14,7 @@ router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet, basename='comments'
 )
+router.register(r'users', UserViewSet)
 
 
 urlpatterns = [
