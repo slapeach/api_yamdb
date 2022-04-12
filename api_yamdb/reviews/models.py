2de6d5416f640891dde0c9ecb5994f7e8c7a4ec8
Алексей Жигалов <avzh79@gmail.com>
2022-04-12 09:18:09 +0300

    User model created

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-print('И тебе привет!!!')
-'Privet'
-# Create your models here.
-# New_string
-print("Что-то")
+ROLE_LIST = [
+    ('user', 'пользователь'),
+    ('admin', 'администратор'),
+    ('moderator', 'модератор')
+]
+class User(AbstractUser):
+    bio = models.TextField('Биография', blank=True)
+    role = models.CharField(max_length=20, choices=ROLE_LIST, default='user')
